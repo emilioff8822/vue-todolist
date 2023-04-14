4bcd3008f13d092920ec5b3f5a4c7b8e5ba9379a
emilioff8822 <emilioff@hotmail.it>
2023-04-14 15:05:53 +0200

    commit-js-linetrough-css

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ createApp({
   },
 
   methods: {
-    
+
     // Metodo per aggiungere un nuovo task
     addTask() {
       // Controlla se la lunghezza del nuovo task è inferiore a 5 caratteri
@@ -30,6 +30,29 @@ createApp({
         this.newTask = "";
       }
     },
+
+    //metodo per rimuovere un task
+    removeTask (index) {
+    // controlla se un task selezionato e' stato completato  
+    if(this.tasks[index].done){
+    // lo rimuove dall'array tasks
+    this.tasks.splice(index,1);
+    
+    } else {
+      // non ha la classe done compare il messaggio di errore
+    this.errorMsg = "Attenzione puoi eliminare il task solo se e' stato fatto";
+    
+    // il messaggio di errore sparisce dopo 2 secondi
+
+    setTimeout (()=>{
+    this.errorMsg = "";  
+    },2000);
+
+
+    }
+    }
+
+
 
   
   
