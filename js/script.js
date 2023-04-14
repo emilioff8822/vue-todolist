0a9020bdc68828bfd01f146e042de92f8a0dca9b
emilioff8822 <emilioff@hotmail.it>
2023-04-14 14:03:29 +0200

    commit-comments-js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,39 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      newTask: "",
-      tasks: [],
-      errorMsg: "",
+      newTask: "", 
+      tasks: [], 
+      errorMsg: "", 
     };
   },
+
+  methods: {
+    
+    // Metodo per aggiungere un nuovo task
+    addTask() {
+      // Controlla se la lunghezza del nuovo task è inferiore a 5 caratteri
+      if (this.newTask.length < 5) {
+        // Imposta il messaggio d'errore
+        this.errorMsg = "Il testo deve avere almeno 5 caratteri.";
+
+        // Rimuove il messaggio d'errore dopo 2 secondi
+        setTimeout(() => {
+          this.errorMsg = "";
+        }, 2000);
+      } else {
+        // Aggiunge il nuovo task all'inizio dell'array "tasks"
+        this.tasks.unshift({ text: this.newTask, done: false });
+
+        // Svuota il campo di input del task
+        this.newTask = "";
+      }
+    },
+
+  
+  
+
+
+  }
 
   
 }).mount("#app");
